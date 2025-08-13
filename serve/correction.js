c7e5ee604a237b25a28c4b5787531ac356776de4
jpk <x@a>
2025-08-13 11:09:40 +0900

    add last status

--- serve/correction.js
+++ serve/correction.js
@@ -4,7 +4,7 @@ import { InitDBOnce, AddHistory, SetWord } from './db.js'
 async function main(params) {
     const db = await InitDBOnce()
 
-    console.log(db.data)
+    console.log(db.data.words)
 
     db.data.history.forEach(v => {
         v.list.forEach(v => {
