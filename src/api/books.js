822711feabc40e0340b43a8c0e7738dd1b36fa92
jpk <x@a>
2025-09-10 00:48:42 +0900

    boo

--- src/api/books.js
+++ src/api/books.js
@@ -2,7 +2,8 @@
 let _book = {};
 export async function GetBook(name) {
     if (!_book[name]) {
-        _book[name] = (await import("/public/book/"+name+".json")).default
+         const res = await fetch("/book/"+name+".json")
+        _book[name] = await res.json() // (await import("/public/book/"+name+".json")).default
 
     }
     return _book[name]
